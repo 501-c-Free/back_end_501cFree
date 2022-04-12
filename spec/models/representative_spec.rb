a97e514161eeff04c5c880e8155bbc1c5445ae2a
Michael Brandt <msbrandt00@gmail.com>
2022-04-12 17:09:09 -0600

    Add validations and testing

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Representative do
     describe 'associations' do 
         it { should belong_to(:non_profit) }
     end
+    describe 'validations' do 
+        it { should validate_presence_of(:name) }
+        it { should validate_presence_of(:email) }
+    end
 end

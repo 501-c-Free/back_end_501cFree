0da0846451e8d9276a73ee2f1ce7e367af6ba3fa
Michael Brandt <msbrandt00@gmail.com>
2022-04-14 11:43:07 -0600

    Test facade, poro, service for Charity Navigator

--- app/poros/NGO.rb
+++ app/poros/NGO.rb
@@ -1,10 +1,17 @@
 class NGO 
+    attr_reader :mission, :charity_info_url, :tagline, :website_url, :charity_name, :ein, :state, :city, :zipcode, :street_adress, :country, :category
     def initialize(data) 
         @mission = data[:mission]
-        @website_url = data[:websiteUrl]
-        @charity_name = data[:charityName]
-        @ein = data[:organization][:ein]
-        @address = data[:mailingAddress]
-        @category = data[:classification]
+        @charity_info_url = data[:charityNavigatorURL]
+        @tagline = data[:tagLine]
+        @website_url = data[:websiteURL]
+        @charity_name = data[:charityName].titleize
+        @ein = data[:ein]
+        @state = data[:mailingAddress][:stateOrProvince]
+        @city = data[:mailingAddress][:city].titleize
+        @zipcode = data[:mailingAddress][:postalCode]
+        @street_adress = data[:mailingAddress][:streetAddress1].titleize
+        @country = data[:mailingAddress][:country].nil? ? "USA" : data[:mailingAddress][:country]
+        @category = data[:irsClassification][:nteeClassification]
     end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,3 +68,10 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('DONT_SHARE_MY_PROPUBLIC_SECRET_KEY') { ENV['charity_navigator_secret'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+end

807e76090c358387bc43324c49ba09ce5d3009af
Michael Brandt <msbrandt00@gmail.com>
2022-04-19 15:55:52 -0600

    Reformat user dashboard action to have serializer response

--- spec/requests/api/v1/non_profits_spec.rb
+++ spec/requests/api/v1/non_profits_spec.rb
@@ -22,6 +22,26 @@ RSpec.describe 'non profit requests' do
         post '/api/v1/non_profits', params: {current_user: User.first.id, ein: 530196605 }
         get "/api/v1/non_profits/#{User.last.id}"
         response_info = JSON.parse(response.body, symbolize_names: true)
-        expect(response_info[:ein]).to eq(530196605)
+        expect(response_info[:data][:attributes][:non_profit][:ein]).to eq(530196605)
+    end
+    it 'will show projects that belong to a non profit', :vcr do 
+        post "/api/v1/log_in", params: {email: 'test@yahoo.com', name: 'Johnny Appleseed'}
+        post '/api/v1/create_representative', params: {user_id: User.first.id, type_of_user: 'representative'}
+        post '/api/v1/non_profits', params: {current_user: User.first.id, ein: 530196605 }
+        post '/api/v1/projects', params: {project_name: 'name', description: 'project description', user_id: User.first.id}
+        get "/api/v1/non_profits/#{User.last.id}"
+        response_info = JSON.parse(response.body, symbolize_names: true)
+        expect(response_info[:data][:attributes][:projects].first).to include(:id, :project_name, :description, :non_profit_id)
+    end
+    it 'will have all projects that a non profit has', :vcr do 
+        post "/api/v1/log_in", params: {email: 'test@yahoo.com', name: 'Johnny Appleseed'}
+        post '/api/v1/create_representative', params: {user_id: User.first.id, type_of_user: 'representative'}
+        post '/api/v1/non_profits', params: {current_user: User.first.id, ein: 530196605 }
+        post '/api/v1/projects', params: {project_name: 'name', description: 'project description', user_id: User.first.id}
+        post '/api/v1/projects', params: {project_name: 'name2', description: 'project description2', user_id: User.first.id}
+        get "/api/v1/non_profits/#{User.last.id}"
+        response_info = JSON.parse(response.body, symbolize_names: true)
+        binding.pry
+        expect(response_info[:data][:attributes][:projects].length).to eq(2)
     end
 end

b4a11d906386fd021e9faaabbb66455a297ce505
Michael Brandt <msbrandt00@gmail.com>
2022-04-20 15:25:22 -0600

    Remove unecessary logged in route, add projectdeveloper route

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
         post :create_developer, to: "registrations#developer"
         post :create_representative, to: "registrations#representative"
         delete :logout, to: "sessions#logout" 
-        get :logged_in, to: "sessions#logged_in"
         post :log_in, to: "sessions#create"
         resources :projects
         resources :non_profits
+        resources :project_developers, only: [:create]
         get '/developer/:id/projects', to: "developers#projects"
         get '/developer/', to: "developers#index"
         get '/developer/:id/', to: "developers#show"

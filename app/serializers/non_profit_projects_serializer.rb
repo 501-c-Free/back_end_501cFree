a4b86d7f0fa1a74da261b4c698c7941e033d7178
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-20 13:27:22 -0400

    Feat: add calendly to json serializer

--- app/serializers/non_profit_projects_serializer.rb
+++ app/serializers/non_profit_projects_serializer.rb
@@ -1,25 +1,26 @@
 class NonProfitProjectsSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :non_profit do |object| 
+  attributes :non_profit do |object|
     {
       non_profit_name: object.non_profit_name,
-      street_address: object.street_address, 
+      street_address: object.street_address,
       city: object.city,
-      state: object.state, 
-      representative_name: object.representative_name, 
-      representative_email: object.representative_email, 
-      category: object.category, 
-      ein: object.ein, 
+      state: object.state,
+      representative_name: object.representative_name,
+      representative_email: object.representative_email,
+      category: object.category,
+      ein: object.ein,
       mission: object.mission,
-      website_url: object.website_url, 
-      tagline: object.tagline, 
-      zipcode: object.zipcode, 
-      charity_info_url: object.charity_info_url, 
-      country: object.country
+      website_url: object.website_url,
+      tagline: object.tagline,
+      zipcode: object.zipcode,
+      charity_info_url: object.charity_info_url,
+      country: object.country,
+      calendly: object.calendly
     }
   end
-  attributes :projects do |object| 
-    object.projects.each do |project| 
+  attributes :projects do |object|
+    object.projects.each do |project|
       {
         project_name: project.project_name,
         project_description: project.description

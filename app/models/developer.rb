635249619e60d5ee78c34a4b571bfd3dcdcc8441
Michael Brandt <msbrandt00@gmail.com>
2022-04-21 14:42:29 -0600

    Project show work

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,5 +1,6 @@
 class Developer < ApplicationRecord 
     has_many :project_developers
     has_many :projects, through: :project_developers
+    has_one :user
     validates :name, :email, presence: true
 end

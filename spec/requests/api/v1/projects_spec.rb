5e742ee3fa744869e9aad755c459ba5ca57bc86a
Michael Brandt <msbrandt00@gmail.com>
2022-04-21 17:23:28 -0600

    Additional testing

--- spec/requests/api/v1/projects_spec.rb
+++ spec/requests/api/v1/projects_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Projects requests' do 
     before :each do 
         user_1 = User.create!(name: 'rep', email: 'rep@rep.com')
-        non_profit = NonProfit.create!(non_profit_name: 'rep', representative_email: 'rep@rep.com', representative_name: 'rep')
+        non_profit = NonProfit.create!(non_profit_name: 'rep', representative_email: 'rep@rep.com', representative_name: 'rep', user: user_1)
         user_1.non_profit_id = non_profit.id
         @project_1 = Project.create!(project_name: 'my first project', description: 'project 1 description', non_profit: non_profit)
         user_2 = User.create!(name: 'rep2', email: 'rep2@rep.com')
-        non_profit_2 = NonProfit.create!(non_profit_name: 'rep2', representative_name: 'rep2', representative_email: 'rep2@rep.com')
+        non_profit_2 = NonProfit.create!(non_profit_name: 'rep2', representative_name: 'rep2', representative_email: 'rep2@rep.com', user: user_2)
         user_2.non_profit_id = non_profit_2.id
         project_2 = Project.create!(project_name: 'my first project', description: 'project 1 description', non_profit: non_profit_2)
     end
@@ -31,5 +31,20 @@ RSpec.describe 'Projects requests' do
         response_info = JSON.parse(response.body, symbolize_names: true)
         expect(response_info[:data][:attributes][:project_name]).to eq('my first project')
         expect(response_info[:data][:attributes][:developers].first).to include(:id, :name, :email, :github, :linkedin)    
+    end
+    describe 'updating a project' do 
+        it 'will update the name and description of a project' do 
+            patch "/api/v1/projects/#{@project_1.id}", params: {name: 'new name', description: 'a new description', developer_id: ''}
+            response_info = JSON.parse(response.body, symbolize_names: true)
+            expect(Project.find(@project_1.id).project_name).to eq('new name')
+            expect(Project.find(@project_1.id).description).to eq('a new description')
+        end
+        it 'will add a developer to a project' do 
+            user_3 = User.create(name: 'user', email: 'user@yahoo.com')
+            developer_1 = Developer.create(name: 'user', email: 'user@yahoo.com')
+            patch "/api/v1/projects/#{@project_1.id}", params: {name: 'new name', description: 'a new description', developer_id: developer_1.id}
+            response_info = JSON.parse(response.body, symbolize_names: true)
+            expect(Project.first.developers.first).to eq(developer_1)
+        end
     end
 end
